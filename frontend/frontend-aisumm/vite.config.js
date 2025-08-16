0a2b3f4fa554c928d94caa8150e53bbcdf0e6083
Minhaz Uddin Bhuiyan <minhazuddin3rd@gmail.com>
2025-08-17 03:58:17 +0600

    Update vite.config.js

--- frontend/frontend-aisumm/vite.config.js
+++ frontend/frontend-aisumm/vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
-  },
-});
+  base: '/'  // ensures assets are loaded from root
+})
+
